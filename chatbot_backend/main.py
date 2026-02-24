4d0511c5c980021af0ea80428ef417a7cc8f6243
hanhan <wuhanhan999@gmail.com>
2026-02-24 18:19:35 -0500

    fix DBURL

--- chatbot_backend/main.py
+++ chatbot_backend/main.py
@@ -26,9 +26,18 @@ async def run_rags(request: DatasetRequest):
 
 
 DATABASE_URL = os.getenv("DATABASE_URL_PRIVATE")
-engine = create_async_engine(
-    DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+DATABASE_URL = DATABASE_URL.replace(
+    "postgresql://",
+    "postgresql+asyncpg://",
+    1
 )
+engine = create_async_engine(DATABASE_URL)
 
 @app.get("/debug/embeddings")
 async def check_embeddings():
